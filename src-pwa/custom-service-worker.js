4f1edd0e840e42d679fed452962be3b77737d9e9
Christian Montoya <christian.montoya.b@gmail.com>
2024-07-12 22:26:58 -0400

    Initial Inject Manifest for Service Worker

--- src-pwa/custom-service-worker.js
+++ src-pwa/custom-service-worker.js
@@ -4,25 +4,27 @@
  * quasar.config.js > pwa > workboxMode is set to "injectManifest"
  */
 
+console.log('CUSTOM SERVICE WORKER');
+
 import { clientsClaim } from 'workbox-core'
 import { precacheAndRoute, cleanupOutdatedCaches, createHandlerBoundToURL } from 'workbox-precaching'
 import { registerRoute, NavigationRoute } from 'workbox-routing'
 
-self.skipWaiting()
-clientsClaim()
+// self.skipWaiting()
+// clientsClaim()
 
-// Use with precache injection
+// Use with precache injection set in quasar config file as InjectManifest workbox mode
 precacheAndRoute(self.__WB_MANIFEST)
 
-cleanupOutdatedCaches()
+// cleanupOutdatedCaches()
 
-// Non-SSR fallback to index.html
-// Production SSR fallback to offline.html (except for dev)
-if (process.env.MODE !== 'ssr' || process.env.PROD) {
-  registerRoute(
-    new NavigationRoute(
-      createHandlerBoundToURL(process.env.PWA_FALLBACK_HTML),
-      { denylist: [/sw\.js$/, /workbox-(.)*\.js$/] }
-    )
-  )
-}
+// // Non-SSR fallback to index.html
+// // Production SSR fallback to offline.html (except for dev)
+// if (process.env.MODE !== 'ssr' || process.env.PROD) {
+//   registerRoute(
+//     new NavigationRoute(
+//       createHandlerBoundToURL(process.env.PWA_FALLBACK_HTML),
+//       { denylist: [/sw\.js$/, /workbox-(.)*\.js$/] }
+//     )
+//   )
+// }
